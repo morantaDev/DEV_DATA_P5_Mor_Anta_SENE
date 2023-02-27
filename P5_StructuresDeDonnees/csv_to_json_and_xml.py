9058b8de3980aaa1bf9c35333c9a23c5d191b60f
morantaDev <morantaDev@gmail.com>
2023-02-27 02:28:31 +0000

    new repo

--- P5_StructuresDeDonnees/csv_to_json_and_xml.py
+++ P5_StructuresDeDonnees/csv_to_json_and_xml.py
@@ -2,135 +2,219 @@ import json
 import csv
 import function_json
 
+import xml.etree.ElementTree as ET
 
 print()
 print("Avec quel type de fichier souhaitez-vous travailler?")
 print("Entrer:\n1 pour le csv\n2 pour le json\n3 pour le xml")
 choix=int(input())
+
+
 print()
 
 
 data=[]
+valide=[]
+invalide=[]
+tabValid=[]
+tabInValid=[]
 if choix==1:
+    print("Afficher les valides sous format:")
+    print("Entrer:\n1 pour le json\n2 pour le xml")
+    format=int(input())
+    
+    if format==1:
 
-    with open("/home/moranta/Downloads/Donnees_Projet_Python_DataC5(1).csv",'r') as fichier:
-        lecteur=csv.DictReader(fichier)
-        for row in lecteur:
-            data.append(row)
-            
-        tabValid=[]
-        tabInValid=[]
-            
-    for i in data:
-        if function_json.check_Numero(i) and function_json.Check_Nom(i) and function_json.Check_prenom(i) and function_json.Check_Classe(i) and function_json.Check_Note(i):
-            tabValid.append(i)
-            
-        else:
-            tabInValid.append(i)
-            
-    valide=[]
-    invalide=[]
-    for i in tabValid:
-        valide.append(function_json.calcul(i))
+        with open("/home/moranta/Downloads/Donnees_Projet_Python_DataC5(1).csv",'r') as fichier:
+            lecteur=csv.DictReader(fichier)
+            for row in lecteur:
+                data.append(row)
+                
+            tabValid=[]
+            tabInValid=[]
+                
+        for i in data:
+            if function_json.check_Numero(i) and function_json.Check_Nom(i) and function_json.Check_prenom(i) and function_json.Check_Classe(i) and function_json.Check_Note(i):
+                tabValid.append(i)
+                
+            else:
+                tabInValid.append(i)
+                
+                
+        for i in tabValid:
+            valide.append(function_json.calcul(i))
 
-    # for j in tabInValid:
-    #     invalide.append(function_json.calcul(j))
+        for j in tabInValid:
+            invalide.append(j)
+            
+
+        tri=sorted(valide, key=lambda x: x['Moyenne'], reverse=True)
+
+        with open("json_file", "w") as jsonFile:
+            jsonFile.write(json.dumps(valide, indent=4, ensure_ascii=False))
+
+        #Problème pour récuperer les notes de math, de Français...
+        def convert_rows(items):
+                return """<etudiant>
+                <Numero id="%s">
+                    <Nom>%s</Nom>
+                    <Prénom>%s</Prénom>
+                    <Date_de_naissance>%s</Date_de_naissance>
+                    <Classe>%s</Classe>
+                    <Note>%s</Note>
+                </Numero>
+            </etudiant>
+        """ % (
+            items['Numero'], items['Nom'], items['Prénom'], items['Date de naissance'],items['Classe'], items['Note'])
+
+        with open("xml_file.xml", "w") as xml_file:
+            xml_file.write('\n'.join(convert_rows(dictio) for dictio in tabInValid))
+
+    elif format==2:
+        #Problème pour récuperer les notes de math, de Français...
         
+        with open("/home/moranta/Downloads/Donnees_Projet_Python_DataC5(1).csv",'r') as fichier:
+            lecteur=csv.DictReader(fichier)
+            for row in lecteur:
+                data.append(row)
+                
+            tabValid=[]
+            tabInValid=[]
+                
+        for i in data:
+            if function_json.check_Numero(i) and function_json.Check_Nom(i) and function_json.Check_prenom(i) and function_json.Check_Classe(i) and function_json.Check_Note(i):
+                tabValid.append(i)
+                
+            else:
+                tabInValid.append(i)
+                
+                
+        for i in tabValid:
+            valide.append(function_json.calcul(i))
 
-    tri=sorted(valide, key=lambda x: x['Moyenne'], reverse=True)
+        for j in tabInValid:
+            invalide.append(j)
+        
+        
+        def convert_rows(items):
+                return """<etudiant>
+                <Numero id="%s">
+                    <Nom>%s</Nom>
+                    <Prénom>%s</Prénom>
+                    <Date_de_naissance>%s</Date_de_naissance>
+                    <Classe>%s</Classe>
+                    <Note>%s</Note>
+                </Numero>
+            </etudiant>
+        """ % (
+            items['Numero'], items['Nom'], items['Prénom'], items['Date de naissance'],items['Classe'], items['Matieres'])
 
-    with open("json_file", "w") as jsonFile:
-        jsonFile.write(json.dumps(valide, indent=4, ensure_ascii=False))
+        with open("xml_file.xml", "w") as xml_file:
+            xml_file.write('\n'.join(convert_rows(dictio) for dictio in valide))
+        
+        with open("json_file", "w") as json_File:
+            json_File.write(json.dumps(invalide, indent=4, ensure_ascii=False))
 
-    #Problème pour récuperer les notes de math, de Français...
-    def convert_rows(items):
+elif choix==2:
+    print("Afficher les valides sous format:")
+    print("Entrer:\n1 pour le csv\n2 pour le xml")
+    format=int(input())
+    if format==1:
+        with open('json_database', 'r') as f:
+            data=json.load(f)
+        
+            for i in data:
+                if function_json.check_Numero(i) and function_json.Check_Nom(i) and function_json.Check_prenom(i) and function_json.Check_Classe(i) and function_json.Check_Note(i):
+                    tabValid.append(i)
+                
+            else:
+                tabInValid.append(i)
+                
+        valide=[]
+        invalide=[]
+        for i in tabValid:
+            valide.append(function_json.calcul(i))
+        #print(data)
+        
+        print(valide)
+        
+        for i in tabInValid:
+            invalide.append(i)
+        
+        def convert_rows(items):
             return """<etudiant>
             <Numero id="%s">
                 <Nom>%s</Nom>
                 <Prénom>%s</Prénom>
                 <Date_de_naissance>%s</Date_de_naissance>
                 <Classe>%s</Classe>
-                <Note>%s</Note>
+                <Matieres>%s</Matieres>
             </Numero>
         </etudiant>
     """ % (
-        items['Numero'], items['Nom'], items['Prénom'], items['Date de naissance'],items['Classe'], items['Note'])
+        items['Numero'], items['Nom'], items['Prénom'], items['Date de naissance'],items['Classe'], items['Matieres'])
 
-    with open("xml_file.xml", "w") as xml_file:
-        xml_file.write('\n'.join(convert_rows(dictio) for dictio in tabInValid))
+            """etudiants = etree.Element("etudiants")
+                etudiant = etree.SubElement(etudiants, "etudiant")
+                etudiant.set('id', dict['Numero'])
+                numero = etree.subElement(etudiant, "Numero")
+                numero.text = dict['Numero']
+                nom = etree.SubElement(etudiant, "Nom")
+                nom.text = dict['Nom']
+                prenom = etree.SubElement(etudiant, "Prénom")
+                prenom.text = dict['Prénom']
+                Classe = etree.SubElement(etudiant, "Classe")
+                Classe.text = dict['Classe']
+                Note = etree.SubElement(etudiant, "Note")
+                Note.text = dict['Note']
+                print(etree.tostring(etudiants, pretty_print=True))
+                
+            """
+        with open("xml_file.xml", "w") as xml_file:
+            xml_file.write('\n'.join(convert_rows(dictio) for dictio in invalide))
         
-elif choix==2:
-    tabValid=[]
-    tabInValid=[]
-    with open('json_database', 'r') as f:
-        data=json.load(f)
-    
-        for i in data:
-            if function_json.check_Numero(i) and function_json.Check_Nom(i) and function_json.Check_prenom(i) and function_json.Check_Classe(i) and function_json.Check_Note(i):
-                tabValid.append(i)
-            
-        else:
-            tabInValid.append(i)
-            
-    valide=[]
-    invalide=[]
-    for i in tabValid:
-        valide.append(function_json.calcul(i))
-    #print(data)
-    
-    print(valide)
-    
-    for i in tabInValid:
-        invalide.append(i)
-    
-    def convert_rows(items):
-        return """<etudiant>
-        <Numero id="%s">
-            <Nom>%s</Nom>
-            <Prénom>%s</Prénom>
-            <Date_de_naissance>%s</Date_de_naissance>
-            <Classe>%s</Classe>
-            <Matieres>%s</Matieres>
-        </Numero>
-    </etudiant>
-""" % (
-    items['Numero'], items['Nom'], items['Prénom'], items['Date de naissance'],items['Classe'], items['Matieres'])
+    elif format==2:
+        
+        def convert_rows(items):
+            return """<etudiant>
+            <Numero id="%s">
+                <Nom>%s</Nom>
+                <Prénom>%s</Prénom>
+                <Date_de_naissance>%s</Date_de_naissance>
+                <Classe>%s</Classe>
+                <Matieres>%s</Matieres>
+            </Numero>
+        </etudiant>
+    """ % (
+        items['Numero'], items['Nom'], items['Prénom'], items['Date de naissance'],items['Classe'], items['Matieres'])
 
-        """etudiants = etree.Element("etudiants")
-            etudiant = etree.SubElement(etudiants, "etudiant")
-            etudiant.set('id', dict['Numero'])
-            numero = etree.subElement(etudiant, "Numero")
-            numero.text = dict['Numero']
-            nom = etree.SubElement(etudiant, "Nom")
-            nom.text = dict['Nom']
-            prenom = etree.SubElement(etudiant, "Prénom")
-            prenom.text = dict['Prénom']
-            Classe = etree.SubElement(etudiant, "Classe")
-            Classe.text = dict['Classe']
-            Note = etree.SubElement(etudiant, "Note")
-            Note.text = dict['Note']
-            print(etree.tostring(etudiants, pretty_print=True))
-            
-        """
-    
-    data1=[]
-    with open("xml_file.xml", "w") as xml_file:
-        xml_file.write('\n'.join(convert_rows(dictio) for dictio in valide))
-    
-    
-    #Retourner un fichier csv pour les invalide
-    
-    with open('data_csv', 'w') as csv_file:
-        csvfile = csv.writer(csv_file)
+         
+        with open("xml_file.xml", "w") as xml_file:
+            xml_file.write('\n'.join(convert_rows(dictio) for dictio in valide))
+        data1=[]
         
-    for item in csvfile:
-        data1.append(item)
+        #Retourner un fichier csv pour les invalide
+        
+        with open('data_csv', 'w') as csv_file:
+            csvfile = csv.writer(csv_file)
+            
+        for item in csvfile:
+            data1.append(item)
         
     
     
     
 elif choix==3:
-    pass
+    def xml_to_csv(file_path, data_csv) -> None:
+        tree = ET.parse(file_path)
+        root =tree.getroot()
+        
+        
+        with open(data_csv, 'w') as csv_file:
+            writer = csv.writer(csv_file)
+            
+        for item in writer:
+            pass
 
 
 else:
