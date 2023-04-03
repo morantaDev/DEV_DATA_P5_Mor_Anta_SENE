477f1100fc6077dd034f825055d4dd7f9327ec0a
morantaDev <morantaDev@gmail.com>
2023-04-03 13:44:21 +0000

    new commit

--- javaScript/TP5/index.js
+++ javaScript/TP5/index.js
@@ -48,15 +48,63 @@ const tableau_questions = [
     
     
 for(let i = 0; i<tableau_questions.length; i++){
-        const card = document.createElement("div")
-        card.classList.add("cadre")
 
-        const question = tableau_questions[i]
-            console.log(question.question)
-            card.innerHTML =`<div class="ask"><header>${question.question}</header><div><div class="choice"><input type="radio" id="java" name="java" value="java"checked><label for="java">${question.a}</label></div><div class="choice"><input type="radio" id="c" name="c" value="c"><label for="c">${question.b}</label></div><div class="choice"><input type="radio" id="python" name="python" value="python"><label for="python">${question.c}</label></div><div class="choice"><input type="radio" id="javascript" name="javascript" value="javascript"><label for="javascript">${question.d}</label></div></div></div><div class="button"><button class="btn">Suivant</button></div>`
-            const button = card.querySelector(".button button")
-            console.log(button)
-            button.disabled = true
+    //création de la carte
+    const ma_question = tableau_questions[i]
+
+    const card = document.createElement("div");
+    card.classList.add("cadre");
+    
+    const ask = document.createElement("div");
+    ask.setAttribute('id', 'ask');
+    card.appendChild(ask);
+    
+    const header = document.createElement("header");
+    header.textContent = ma_question.question;
+    ask.appendChild(header);
+    
+    const choices = document.createElement("div");
+    choices.setAttribute('id', 'choices');
+    
+    for (let i = 0; i < 4; i++) {
+    const choice = document.createElement("div");
+    choice.classList.add('choice');
+    
+    const input = document.createElement("input");
+    input.setAttribute("type", "radio");
+    input.classList.add("input");
+    //   input.setAttribute("name", "");
+    input.setAttribute("value", ma_question[String.fromCharCode(97 + i)]);
+        
+    const label = document.createElement("label")
+    label.setAttribute("for", "")
+
+    const inputContent = document.createTextNode(ma_question[String.fromCharCode(97 + i)]);
+        
+    label.appendChild(inputContent)
+    choice.appendChild(input);
+    choice.appendChild(label);
+    choices.appendChild(choice);
+
+    
+    ask.appendChild(choices)
+    }
+        const button1 = document.createElement("div")
+        button1.classList.add('button')
+        const btn = document.createElement('button')
+        btn.classList.add("btn")
+        btn.textContent = "Suivant"
+        button1.appendChild(btn)
+        card.appendChild(button1)
+        console.log(card);
+    
+    
+
+        // console.log(ma_question.question)
+        // card.innerHTML =`<div class="ask"><header>${ma_question.question}</header><div><div class="choice"><input type="radio" id="java" name="java" value="java"><label for="java">${ma_question.a}</label></div><div class="choice"><input type="radio" id="c" name="c" value="c"><label for="c">${ma_question.b}</label></div><div class="choice"><input type="radio" id="python" name="python" value="python"><label for="python">${ma_question.c}</label></div><div class="choice"><input type="radio" id="javascript" name="javascript" value="javascript"><label for="javascript">${ma_question.d}</label></div></div></div><div class="button"><button class="btn">Suivant</button></div>`
+        const button = card.querySelector(".button button")
+        // console.log(button)
+        button.disabled = true
         
         button.addEventListener('click', function(){
                 const nextCard = card.nextElementSibling;
@@ -73,7 +121,13 @@ for(let i = 0; i<tableau_questions.length; i++){
             button.disabled = false;
             card.style.display = "block";
           }
-    }
+
+        }
+        const question1 = tableau_questions[1]
+        console.log(question1.correct)
+        const choix1 = document.querySelector('input')
+        console.log(choix1)
+
 })
 
     
