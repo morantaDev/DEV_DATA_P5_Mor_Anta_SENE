d5b6d305578e10a238450bbe7d99559641ef0348
morantaDev <morantaDev@gmail.com>
2023-07-07 14:45:30 +0000

    new commit

--- web_scraping_JS/app.js
+++ web_scraping_JS/app.js
@@ -3,6 +3,7 @@ const Router = require('koa-router');
 const axios = require('axios');
 const cheerio = require('cheerio');
 const {Pool} = require('pg')
+const puppeteer = require('puppeteer')
 const pretty = require('pretty')
 
 const pool = new Pool({
@@ -18,31 +19,50 @@ async function scrapeAndInsertData(){
         // const response = await axios.get("https://www.terre-net-occasions.fr/tracteur/c1");
         // const response = await axios.get("https://www.agriaffaires.com/occasion/1/tracteur-agricole.html")
         materials=[]
-        for(let index=0; index <= 36; index++){
-            var response = await axios.get(`https://french.alibaba.com/g/china-agricultural-machinery${index}.html`)
+        // for(let index=0; index <= 36; index++){
+        //     var response = await axios.get(`https://french.alibaba.com/g/china-agricultural-machinery${index}.html`)
     
     
-            var $ = cheerio.load(response.data);  //Parser la reponse html en utilisant cheerio
-            // console.log(pretty($.html()));
-            // console.log(pretty($.html()));
-            // console.log($.html());
+        //     var $ = cheerio.load(response.data);  //Parser la reponse html en utilisant cheerio
+        //     // console.log(pretty($.html()));
+        //     // console.log(pretty($.html()));
+        //     // console.log($.html());
     
     
-            $('.product-card').each((index, element) => {
-                const material = {
-                    nom: $(element).find('.product-title span').text(),
-                    image_url: $(element).find('a img').attr('src'),
-                    description: $(element).find('.product-title span:eq(1)').text(),
-                    // categorie=; 
-                    prix: $(element).find('.product-price .price-number').text()
-                };
-                materials.push(material)
-                // console.log(materials)
-            });
+        //     $('.product-card').each((index, element) => {
+        //         const material = {
+        //             nom: $(element).find('.product-title span').text(),
+        //             image_url: $(element).find('a img').attr('src'),
+        //             description: $(element).find('.product-title span:eq(1)').text(),
+        //             // categorie=; 
+        //             prix: $(element).find('.product-price .price-number').text()
+        //         };
+        //         materials.push(material)
+        //         // console.log(materials)
+        //     });
             
-            console.log(materials) 
+        //     console.log(materials) 
 
-        }
+        // }
+        
+        // const response = await axios.get("https://www.alibaba.com/premium/agriculture_machine_machinery.html?p4phangyebuliu=1&src=sem_ggl&field=UG&from=sem_ggl&cmpgn=18084521455&adgrp=143584075809&fditm=&tgt=kwd-1729857182300&locintrst=&locphyscl=9067846&mtchtyp=b&ntwrk=g&device=c&dvcmdl=&creative=619614436792&plcmnt=&plcmntcat=&aceid=&position=&gclid=CjwKCAjwzJmlBhBBEiwAEJyLu_pgB19i9oYbDEjbKPnjfnuFdveWHa7yFZjCjAt0extKpke9GIkGJBoC-nMQAvD_BwE");
+        // const response = await axios.get("https://www.farmmachinerylocator.co.uk/listings/for-sale/planting-equipment/1103");
+        const response = await axios.get("https://www.bossmachinery.nl/en/vehicles?qooqie_creative_id=529003977107&keywords=used%20heavy%20machinery&gad=1&gclid=CjwKCAjwzJmlBhBBEiwAEJyLu_E-a3UkppuQOUCcMuu6UK9BfSuqgiGa1X_9u-zJuhlVCv6F4P-QVxoCCJIQAvD_BwE");
+        const $ = cheerio.load(response.data);
+        // console.log($.html())
+
+        $('.pure-u-xl-1-3').each((index, element)=>{
+            var material = {
+                nom: $(element).find('.vehicle-brand').text(),
+                description: $(element).find('.vehicle-special-feature').text(),
+                prix: $(element).find('.vehicle-price').text(),
+                image_url: $(element).find('img').attr('src')
+            }
+            // console.log(material)
+            materials.push(material)
+        })
+        console.log(materials)
+        
     }catch(e){
         console.log(e)
     }
@@ -88,3 +108,66 @@ app.use(router.routes());
 // app.use(router.routes()).use(router.allowedMethods());
 
 module.exports =app;
+
+
+
+
+
+
+
+// const axios = require('axios');
+// const cheerio = require('cheerio');
+// const puppeteer = require('puppeteer');
+
+// async function scrapeAndScroll() {
+//   try {
+//     const browser = await puppeteer.launch();
+//     const page = await browser.newPage();
+//     await page.goto("https://www.bossmachinery.nl/en/vehicles?qooqie_creative_id=529003977107&keywords=used%20heavy%20machinery&gad=1&gclid=CjwKCAjwzJmlBhBBEiwAEJyLu_E-a3UkppuQOUCcMuu6UK9BfSuqgiGa1X_9u-zJuhlVCv6F4P-QVxoCCJIQAvD_BwE");
+
+//     // Scroll to the bottom of the page
+//     await autoScroll(page);
+
+//     // Get the updated HTML content after scrolling
+//     const htmlContent = await page.content();
+//     const $ = cheerio.load(htmlContent);
+//     const materials = [];
+
+//     $('.pure-u-xl-1-3').each((index, element) => {
+//       const material = {
+//         nom: $(element).find('.vehicle-brand').text(),
+//         description: $(element).find('.vehicle-special-feature').text(),
+//         prix: $(element).find('.vehicle-price').text(),
+//         image_url: $(element).find('img').attr('src')
+//       };
+//       materials.push(material);
+//     });
+
+//     console.log(materials);
+
+//     // Close the browser
+//     await browser.close();
+//   } catch (e) {
+//     console.log(e);
+//   }
+// }
+
+// async function autoScroll(page) {
+//   await page.evaluate(async () => {
+//     await new Promise((resolve, reject) => {
+//       let totalHeight = 0;
+//       const distance = 100;
+//       const timer = setInterval(() => {
+//         const scrollHeight = document.documentElement.scrollHeight;
+//         window.scrollBy(0, distance);
+//         totalHeight += distance;
+//         if (totalHeight >= scrollHeight) {
+//           clearInterval(timer);
+//           resolve();
+//         }
+//       }, 100);
+//     });
+//   });
+// }
+
+// scrapeAndScroll();
